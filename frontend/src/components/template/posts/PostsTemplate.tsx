16da6657c875907182193a85821da896b02599be
SKYE <skye@MacBook-Pro-MacBook.local>
2023-07-08 15:29:03 +0300

    Added loading for posts feeds

--- frontend/src/components/template/posts/PostsTemplate.tsx
+++ frontend/src/components/template/posts/PostsTemplate.tsx
@@ -1,20 +1,24 @@
 'use client';
 import { FC } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 import CreatePost from '@/components/oraganims/post/create-post/CreatePost';
 import PostCard from '@/components/oraganims/post/post-card/PostCard';
 import { useGetPostsQuery } from '@/globalRedux/service/postApi';
-import type { IPost } from '@/types/post';
+import { LoadingFeeds } from './style';
 const PostsTemplate: FC = () => {
-  const { data: posts, isLoading } = useGetPostsQuery();
+  const { data, isLoading } = useGetPostsQuery();
+  const posts = data!;
 
   return (
     <>
       <CreatePost />
       <h4>Feeds</h4>
       {isLoading ? (
-        <p>Loading...</p>
+        <LoadingFeeds>
+          <CircularProgress color="inherit" />
+        </LoadingFeeds>
       ) : (
-        posts?.map((post) => {
+        posts.map((post) => {
           return <PostCard post={post} key={post._id} />;
         })
       )}
---
+++ frontend/src/components/template/posts/style.tsx
@@ -0,0 +1,7 @@
+'use client';
+import styled from 'styled-components';
+
+export const LoadingFeeds = styled.div`
+  display: flex;
+  justify-content: center;
+`;
